4710ba217c5294adfe9fcfe9e52a4034df606aa0
Gaurav Kunal <gkunal7691@gmail.com>
2020-10-22 22:17:16 +0530

    asdasd

--- server.js
+++ server.js
@@ -6,7 +6,17 @@ const app = express();
 // Serve only the static files form the dist directory
 //app.use(express.static(__dirname + '/dist/scrom-example'));
 
+app.get('/', function (req, res) {
+    console.log("working blank");
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    res.sendFile(path.join(__dirname + '/dist/scrom-example/index.html'));
+});
+
 app.get('/*', function (req, res) {
+    console.log("working");
     res.header("Access-Control-Allow-Origin", '*');
     res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
